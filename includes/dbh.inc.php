6a4e46a5592fe2440960a1a8e125473e793e0f6c
randikawann <randikawann@gmail.com>
2018-04-30 13:40:37 -0400

    complete signup with database

---
+++ includes/dbh.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+$bdServername="localhost";
+$bdUserName="root";
+$bdPassword="";
+$bdName="logingsystem";
+
+/*connect database with php file*/
+$conn= mysqli_connect($bdServername, $bdUserName, $bdPassword, $bdName);
+
+
+
--- signup.php
+++ signup.php
@@ -4,7 +4,7 @@
 	<section class="main-container">
 		<div class="main-wrapper">
 			<h2>signup</h2>
-			<form class="signup-form">
+			<form class="signup-form" action="includes/signup.inc.php" method="POST">
 				<input type="text" name="first" placeholder="Firstname">
 				<input type="text" name="last" placeholder="Lastname">
 				<input type="text" name="email" placeholder="E-mail">
