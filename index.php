a889f2a4baf49e9e8c7c558fd2aa66c2179301f4
randikawann <randikawann@gmail.com>
2018-04-30 08:36:57 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body>
+	<header>
+		<nav>
+			<div class="main-wrapper">
+				
+			</div>
+		</nav>
+	</header>
+	<section>
+		
+	</section>
+</body>
+</html>
